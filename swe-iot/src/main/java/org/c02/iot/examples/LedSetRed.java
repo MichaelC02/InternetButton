7cd06aaf72359da126cbeea6ee8a935ad9a5b1c7
teschl <teschl@172.31.35.32>
2016-02-06 12:55:12 +0100

    LedDemoApp hinzugefügt

---
+++ swe-iot/src/main/java/org/c02/iot/examples/LedSetRed.java
@@ -0,0 +1,36 @@
+package org.c02.iot.examples;
+
+import java.awt.Color;
+
+import org.c02.iot.InternetButtonApi;
+import org.c02.iot.InternetButtonApi.ButtonDirection;
+import org.c02.iot.InternetButtonImpl;
+import org.c02.iot.cloud.api.ParticleApiWrapper;
+import org.c02.iot.cloud.api.ParticleApiWrapperImpl;
+import org.c02.iot.cloud.examples.DemoConstants;
+
+public class LedSetRed {
+
+	static ParticleApiWrapper api = new ParticleApiWrapperImpl(DemoConstants.deviceId,
+			DemoConstants.accessToken);
+	
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		InternetButtonApi button = new InternetButtonImpl(api);
+		button.allLedsOff();
+		int count = 0;
+		while (true)
+		{
+			count = button.getButtonCounter(ButtonDirection.North);
+			int red = (count*10)%255;
+			String rgb = String.format("%03d000000", red);
+			button.setLed(1, Color.decode(rgb));
+			Thread.sleep(100);
+			
+		}
+
+		
+	}
+
+}
